1d2c5791b04618fd29493237cee4984b91aa7336
jeffery5bai <76144802+jeffery5bai@users.noreply.github.com>
2025-02-28 01:43:50 +0800

    [TAS-18] build CORS mechanism (#9)

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,10 +3,22 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes.routes import initialize_routes
+from app.core.config import FRONTEND_SERVER_URL, BACKEND_SERVER_URL
 
 
 app = FastAPI(title="RAG-for-fun")
+
+origins = [FRONTEND_SERVER_URL, BACKEND_SERVER_URL]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # 允許哪些來源發起跨域請求
+    allow_credentials=True,
+    allow_methods=["*"],    # 允許所有 HTTP 方法，如 GET、POST、PUT 等
+    allow_headers=["*"],    # 允許所有 Header
+)
 
 # Mount static files (CSS, JS)
 app.mount("/static", StaticFiles(directory="app/views/static"), name="static")
